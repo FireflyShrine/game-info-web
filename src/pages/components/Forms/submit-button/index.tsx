976bfd31461d0c577ccbe02f743187bf7eeb1a8c
Anderson Barbosa <ands.david@live.com>
2021-10-08 21:19:32 -0300

    botão de envio

---
+++ src/pages/components/Forms/submit-button/index.tsx
@@ -0,0 +1,29 @@
+import { Button, ButtonProps } from '@chakra-ui/react'
+import { IoSendOutline } from 'react-icons/io5'
+import { WithChildren } from '../../../../@types/with-children'
+
+interface Props extends ButtonProps {
+  isRequesting?: boolean
+}
+const SubmitButton = ({
+  isRequesting,
+  children,
+  ...rest
+}: WithChildren<Props>) => {
+  return (
+    <Button
+      type="submit"
+      isDisabled={isRequesting}
+      isLoading={isRequesting}
+      colorScheme="blue"
+      variant={rest.variant ?? 'solid'}
+      loadingText={rest.loadingText ?? 'Enviando'}
+      spinnerPlacement={rest.spinnerPlacement ?? 'start'}
+      {...rest}
+    >
+      {children ?? 'Enviar'}
+    </Button>
+  )
+}
+
+export default SubmitButton
