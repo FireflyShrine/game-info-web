56389572d3d71e076f2c587cdbe092a78f55815d
Anderson Barbosa <ands.david@live.com>
2021-11-05 21:49:21 -0300

    preparando hook

---
+++ src/hooks/createSession.ts
@@ -0,0 +1,18 @@
+// this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
+import { NextApiResponse } from 'next'
+import { withIronSession } from 'next-iron-session'
+import { CRequest } from '../../@types/requests-methods'
+
+export default function createSession(
+  handler: (req: CRequest, res: NextApiResponse) => void
+) {
+  return withIronSession(handler, {
+    password: process.env.SECRET_COOKIE_PASSWORD ?? '',
+    cookieName: '@game-info/user',
+    cookieOptions: {
+      // the next line allows to use the session in non-https environments like
+      // Next.js dev mode (http://localhost:3000)
+      secure: process.env.NODE_ENV === 'production' ? true : false,
+    },
+  })
+}
